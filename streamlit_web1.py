c0d45731c9fe7cc21fd5accd3fe30caef7de5531
nornforgood <norn_chang@hotmail.com>
2026-09-02 11:23:25 +0800

    修正內容

--- streamlit_web1.py
+++ streamlit_web1.py
@@ -2,8 +2,7 @@ import streamlit as st
 
 st.title("肺炎研究臨床治療存活率")
 st.divider()
-
-
+st.logo("imges/logo.jpg",size='large')
 col1,col2 =st.columns(2)
 
 with col1:
@@ -17,8 +16,8 @@ with c1:
     st.write("肺癌長年霸佔台灣癌症死亡率第 1 名。造成肺癌高死亡率的主因是早期症狀不明顯，如何能夠早期篩檢肺癌，LDCT 是什麼？肺癌的種類有哪些？台灣常見的肺腺癌又是什麼？肺癌如何診斷？目前肺癌治療策略是什麼？相關懶人包一次報你知")
 
     st.divider()
-    st.write("肺癌長年霸佔台灣癌症死亡率第 1 名。造成肺癌高死亡率的主因是早期症狀不明顯，如何能夠早期篩檢肺癌，LDCT 是什麼？肺癌的種類有哪些？台灣常見的肺腺癌又是什麼？肺癌如何診斷？目前肺癌治療策略是什麼？相關懶人包一次報你知")
-    
+    st.header("powerBI研究畫面")
+    st.image("images/lung.png",caption="肺癌存活率與各期占比圖")
 
 with st.sidebar:
     with st.container():
