bc597a0caa4b6012a5ee6d0cc630c1641415de69
nornforgood <norn_chang@hotmail.com>
2026-09-02 09:42:22 +0800

    init

---
+++ streamlit_web1.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title("肺炎研究臨床治療存活率")
+st.divider()
+
+
+with st.siderbar:
+    st.header("各項分析圖表")
+
+with st.bottom:
+    st.header("關於我")
+    st.text("聯絡資訊: email:")
